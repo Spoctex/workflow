2994874e909e37422485af8788497d73fba9d3b4
Casey <spoocter@gmail.com>
2023-08-24 18:03:03 -0400

    ff

--- backend/db/migrations/20230823181142-create-comment.js
+++ backend/db/migrations/20230823181142-create-comment.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Comments', {
@@ -43,6 +49,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = 'Comments';
     await queryInterface.dropTable('Comments');
   }
 };
