0c55f031b6fb04bd526012ac770c163ccbdaf97e
Casey <spoocter@gmail.com>
2023-09-16 14:10:53 -0400

    added comments showing ideas to keep sidebar on screen while scrolling through issues

--- frontend/src/components/IssueBoard/index.js
+++ frontend/src/components/IssueBoard/index.js
@@ -3,7 +3,7 @@ import './IssueBoard.css';
 
 
 
-function IssueBoard({issArr}) {
+function IssueBoard({ issArr }) {
 
 
 
@@ -15,30 +15,32 @@ function IssueBoard({issArr}) {
     const issDup = issArr.filter(iss => iss[0].status === 'Duplicate');
 
     return (
-        <div id='colCont'>
-            <div>
-                <h2>Backlog</h2>
-                {issBack.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
-            </div>
-            <div>
-                <h2>ToDo</h2>
-                {issToDo.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
-            </div>
-            <div>
-                <h2>In Progress</h2>
-                {issInProg.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
-            </div>
-            <div>
-                <h2>Done</h2>
-                {issDone.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
-            </div>
-            <div>
-                <h2>Canceled</h2>
-                {issCanc.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
-            </div>
-            <div>
-                <h2>Duplicate</h2>
-                {issDup.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+        <div id='issBoard'>
+            <div id='colCont'>
+                <div>
+                    <h2>Backlog</h2>
+                    {issBack.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
+                <div>
+                    <h2>ToDo</h2>
+                    {issToDo.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
+                <div>
+                    <h2>In Progress</h2>
+                    {issInProg.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
+                <div>
+                    <h2>Done</h2>
+                    {issDone.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
+                <div>
+                    <h2>Canceled</h2>
+                    {issCanc.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
+                <div>
+                    <h2>Duplicate</h2>
+                    {issDup.map(iss => (<IssueCard issue={iss[0]} project={iss[1]} team={iss[2]} />))}
+                </div>
             </div>
         </div>
     )
